1371df82f797dad50582bd4577de251cbe9732d6
willmcgowan <mcgowan.wmb@gmail.com>
2021-03-24 05:30:35 +0000

    Update risk.h

--- open_spiel/games/risk.h
+++ open_spiel/games/risk.h
@@ -176,6 +176,7 @@ protected:
   void DoApplyAction(Action move) override;
   int num_terrs_;
   int starting_troops_;
+  int max_turns_;
   std::vector<double> rewards_;
   std::vector<int> assist_;
   std::vector<int> board;

b4d1ad077f36abed94c63f02622335d0fbad103a
willmcgowan <mcgowan.wmb@gmail.com>
2021-03-26 17:20:07 +0000

    small change

--- open_spiel/games/risk.h
+++ open_spiel/games/risk.h
@@ -173,7 +173,9 @@ class RiskState : public State {
   std::string ToString() const override {
       return 0;
   }
-  std::string ActionToString(Player player, Action action_id) const override = 0;
+  std::string ActionToString(Player player, Action action_id) const override {
+      return 0;
+  }
  
 
 protected:
